2f941ec728ecb33ba8f1b8833c715f15f5fcbac0
manami-m7 <kmuuum@gmail.com>
2021-01-25 13:13:49 +0000

    データベース修正のためブランチ新規作成、データベース修正

--- db/migrate/20210121152922_add_group_id_to_group_users.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupIdToGroupUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :group_users, :group_id, :integer
-  end
-end
--- db/migrate/20210122065415_rename_name_column_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RenameNameColumnToTasks < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :tasks, :name, :task_name
-  end
-end
