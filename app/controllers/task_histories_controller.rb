2e2d639319684ec938ee6b271a4381a2c8bd4520
manami-m7 <kmuuum@gmail.com>
2021-01-28 02:33:20 +0000

    task_historiesコントローラ、viewの修正

--- app/controllers/task_histories_controller.rb
+++ app/controllers/task_histories_controller.rb
@@ -3,24 +3,24 @@ class TaskHistoriesController < ApplicationController
 protect_from_forgery :except => [:start]
 
   def new
+    @task_id = params[:task_id]
     @task_history = TaskHistory.new
   end
 
   def start
     @task_history = TaskHistory.new
+    @task_history.task_id = params[:task_id]
     @task_history.start_time = Time.now
     @task_history.user_id = current_user.id
-
     @task_history.save
     render new_task_history_path
 
   end
 
   def finish
-    # byebug
-    @task_history = TaskHistory.find(params[:id])
+    @task_history_id = TaskHistory.where(finish_time: nil)
+    @task_history = TaskHistory.find(params[:task_history_id])
     @task_history.finish_time = Time.now
-    @task_history.user_id = current_user.id
     @task_history.save
     redirect_to task_histories
   end
@@ -38,9 +38,4 @@ protect_from_forgery :except => [:start]
   def destroy
   end
 
-  private
-
-  def task_history_params
-    params.require(:taskhistory).permit(:start_time, :finish_time)
-  end
 end
