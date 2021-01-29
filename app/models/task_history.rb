8892c5b8e269447e143088ff6e9e2d54d3edafc3
manami-m7 <kmuuum@gmail.com>
2021-01-29 07:24:58 +0000

    時間の表示を変更

--- app/models/task_history.rb
+++ app/models/task_history.rb
@@ -1,4 +1,9 @@
 class TaskHistory < ApplicationRecord
   belongs_to :task
   belongs_to :user
+
+  # def time_diff
+  #   @time_diff = task_history.finish_time - task_history.start_time
+  # end
+
 end
