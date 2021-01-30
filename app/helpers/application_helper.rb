a4dc8c610398911ac6aa388ed50d97f306f18e59
manami-m7 <kmuuum@gmail.com>
2021-01-30 06:12:58 +0000

    履歴の時刻編集と削除機能追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def simple_time(time)
-    time.strftime('%m/%d %H:%M')
+    time.strftime('%H:%M')
   end
+
+  def simple_day(day)
+    day.strftime('%m/%d')
+  end
+
 end
