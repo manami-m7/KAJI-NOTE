e2d388a56564b821aff217d06ab0a3528676a546
manami-m7 <kmuuum@gmail.com>
2021-01-30 12:47:47 +0000

    タスクごとの経過時間による円グラフ表示完成

--- app/models/task_history.rb
+++ app/models/task_history.rb
@@ -5,7 +5,9 @@ class TaskHistory < ApplicationRecord
 
 # 小数点以下は切り上げ表示
   def time_diff
-    return ((self.finish_time - self.start_time) / 60).ceil
+    return ((self.finish_time - self.start_time) / 60).round
   end
+
+
 
 end
