bbf7bcd46385b065361ee5dece1d62544095fe16
manami-m7 <kmuuum@gmail.com>
2021-02-05 17:19:18 +0000

    timerページのデザイン、全体のデザイン修正

--- app/models/task_history.rb
+++ app/models/task_history.rb
@@ -2,11 +2,9 @@ class TaskHistory < ApplicationRecord
   belongs_to :task
   belongs_to :user
 
-# 小数点四捨五入
+  # 小数点四捨五入
   def time_diff
     return ((self.finish_time - self.start_time) / 60).round
   end
-
-
 
 end
