bb2e2ab0622f4199fff664399316604a077dbe2e
manami-m7 <kmuuum@gmail.com>
2021-02-10 14:19:46 +0000

    chartを本番環境で表示の修正

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -9,12 +9,13 @@ class ChartsController < ApplicationController
     #   @charts_data[Task.find(t.task_id).task_name] += t.time_diff
     # end
     if Rails.env.production?
+      # finishtime, starttime
       @charts_data = TaskHistory.where(user_id: current_user.id).joins(:task).group(:task_id)
-    .pluck(Arel.sql("task_name,ROUND(SUM((DATE_FORMAT(finish_time, '%s')-DATE_FORMAT(finish_time, '%s'))/60),1)"))
+    .pluck(Arel.sql("task_name,ROUND(SUM(time_to_sec(TIMEDIFF(start_time, finish_time))))"))
     .map{ |th| {th[0]=>th[1]}}.reduce(&:merge)
 
     @charts_average = TaskHistory.where(user_id: current_user.id).joins(:task).group(:task_id)
-    .pluck(Arel.sql("task_name,ROUND(AVG((DATE_FORMAT(finish_time, '%s')-DATE_FORMAT(finish_time, '%s'))/60),1)"))
+    .pluck(Arel.sql("task_name,ROUND(AVG(time_to_sec(TIMEDIFF(start_time, finish_time))))"))
     .map{ |th| {th[0]=>th[1]}}.reduce(&:merge)
     else
       @charts_data = TaskHistory.where(user_id: current_user.id).joins(:task).group(:task_id)
